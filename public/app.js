a743641c2e81dcc81719f5f339e1d379aae98a15
devinsh <devinsh24@gmail.com>
2015-12-02 02:19:05 -0500

    add app.js

---
+++ public/app.js
@@ -0,0 +1,26 @@
+(function(){
+
+  'use strict'
+
+  angular.module('main', ['ngCookies']).controller('LoginCtrl', function($http, $cookies, $scope){
+    var self = this;
+    self.usr = $cookies.get('usr')
+    self.login = function(usr, pwd){
+      $http.post('login', {usr:usr, pwd:pwd}).success(function(usrs){
+	console.log(usrs);
+	if(usrs.length > 0){
+	  delete $scope.loginUser;
+	  delete $scope.loginPwd;
+	  self.usr = usrs[0].usr;
+	  $cookies.put('usr', self.usr);
+	}
+      });
+
+      };
+
+    self.logout = function(){
+      $cookies.remove('usr')
+      self.usr = undefined;
+    }
+  });
+})()
